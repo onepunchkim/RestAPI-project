03897440929eeba0008b58793ec92f79c0210ab6
onepunchkim <hyukjin4711@gmail.com>
2026-07-11 18:51:11 +0900

    add member signup service

--- src/main/java/com/example/board_api/member/service/MemberService.java
+++ src/main/java/com/example/board_api/member/service/MemberService.java
@@ -1,7 +1,11 @@
 package com.example.board_api.member.service;
 
+import com.example.board_api.member.dto.request.MemberCreateRequest;
+import com.example.board_api.member.dto.response.MemberResponse;
+import com.example.board_api.member.entity.Member;
 import com.example.board_api.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,4 +15,35 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public MemberResponse signup(MemberCreateRequest request) {
+        validateDuplicateEmail(request.email());
+        validateDuplicateNickname(request.nickname());
+
+        String encodedPassword = passwordEncoder.encode(request.password());
+
+        Member member = Member.create(
+                request.email(),
+                encodedPassword,
+                request.nickname()
+        );
+
+        Member savedMember = memberRepository.save(member);
+
+        return MemberResponse.from(savedMember);
+    }
+
+    private void validateDuplicateEmail(String email) {
+        if (memberRepository.existsByEmail(email)) {
+            throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
+        }
+    }
+
+    private void validateDuplicateNickname(String nickname) {
+        if (memberRepository.existsByNickname(nickname)) {
+            throw new IllegalArgumentException("이미 존재하는 닉네임입니다.");
+        }
+    }
 }
